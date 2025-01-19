545cc85f4121ce0f8e38a1a6c6fbd3ad82d90e1a
Daniela Herrera Betanzos <herrerabetanzosdaniela7@gmail.com>
2025-01-18 23:46:19 -0800

    changes

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.send('Quiz 4 Daniela Herrera');
 });
 
-app.listen(PORT, () => {
-    console.log(`App is running on http://localhost:${PORT}`);
+app.listen(PORT,'0.0.0.0', () => {
+    console.log(`App is running on http://0.0.0.0:${PORT}`);
 });
